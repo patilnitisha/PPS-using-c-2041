acfc6734f0fadafe4a5c1f77ec3ff2ba685af5cd
patilnitisha <patilnitisha78@gmail.com>
2025-04-30 13:17:51 +0530

    Student marks and display passed subject

---
+++ write a program which accept student marks and displays passed if he has pass the subject .c
@@ -0,0 +1,13 @@
+u#include<stdio.h>
+int main()
+{    
+    int marks;
+    printf("Enter marks:\t");
+    scanf("%d", &marks);
+    if (marks>=40)
+    {
+        printf("PASS");
+    }
+    printf("End of program");
+    return 0;
+}    
