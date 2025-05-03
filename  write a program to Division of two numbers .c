8ab43e1d3a59f2b544b93f6ae0e431e30cdf2038
patilnitisha <patilnitisha78@gmail.com>
2025-05-03 18:34:42 +0530

    Division of two numbers

---
+++  write a program to Division of two numbers .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() 
+{
+    int num1, num2, sum;
+
+    printf("Enter first number:\t");
+    scanf("%d", &num1);
+
+    printf("Enter second number:\t");
+    scanf("%d", &num2);
+    
+    sum = num1/num2;
+
+    printf("sum of %d and %d is %d", num1, num2, sum);
+
+    return 0;
+}
